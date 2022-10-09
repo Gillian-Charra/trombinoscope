8e07a2175e4a47b7d917af274a6b68e64d3f5daf
GillianCharra <gillian@charra.fr>
2022-10-09 22:54:29 +0200

    Trombinoscope fonctionnel

--- vendor/phpalchemy/yaml/Alchemy/Component/Yaml/Tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// PHP Yaml lib. bootstrap
-
-define('HOME_DIR', realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR);
-include_once HOME_DIR . 'Yaml.php';
-
-set_include_path(HOME_DIR . 'Tests/Fixtures' . PATH_SEPARATOR . get_include_path());
-
